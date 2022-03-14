9025eb9764e0e5d79c9416284828218397f39b67
LucAssis <lucassis7@outlook.com>
2022-03-14 19:08:53 -0300

    adicionando exercicios do dia 5.1

---
+++ BL01_FUNDAMENTOS/bloco-5-javascript-dom-eventos-web-storage/script.js
@@ -0,0 +1,32 @@
+let header = document.getElementsByTagName('header');
+
+header[0].style.backgroundColor = '#90be6d';
+
+
+let emergencyTasks = document.getElementsByClassName('emergency-tasks');
+
+for (i = 0; i < emergencyTasks.length; i += 1) {
+    emergencyTasks[i].style.backgroundColor = '#e07a5f';
+}
+
+let noEmergencyTasks = document.getElementsByClassName('no-emergency-tasks');
+
+for (i = 0; i < noEmergencyTasks.length; i += 1) {
+    noEmergencyTasks[i].style.backgroundColor = '#e9c46a';
+}
+
+let emergency = document.querySelectorAll('.emergency-tasks h3');
+
+for (i = 0; i < emergency.length; i += 1) {  
+    emergency[i].style.backgroundColor = 'blue';
+}
+
+let noEmergency = document.querySelectorAll('.no-emergency-tasks h3');
+
+for (i = 0; i < noEmergency.length; i += 1) {  
+    noEmergency[i].style.backgroundColor = 'black';
+}
+
+let footer = document.getElementsByTagName('footer');
+
+footer[0].style.backgroundColor = '#081c15';
