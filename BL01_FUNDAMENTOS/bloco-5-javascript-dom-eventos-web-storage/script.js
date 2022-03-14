037577b1bc8950a94c361e77dcb950db33c7145c
LucAssis <lucassis7@outlook.com>
2022-03-14 20:02:03 -0300

    atualizaçao dos exercicios do dia 5.1

--- BL01_FUNDAMENTOS/bloco-5-javascript-dom-eventos-web-storage/script.js
+++ BL01_FUNDAMENTOS/bloco-5-javascript-dom-eventos-web-storage/script.js
@@ -5,25 +5,25 @@ header[0].style.backgroundColor = '#90be6d';
 
 let emergencyTasks = document.getElementsByClassName('emergency-tasks');
 
-for (i = 0; i < emergencyTasks.length; i += 1) {
+for (let i = 0; i < emergencyTasks.length; i += 1) {
     emergencyTasks[i].style.backgroundColor = '#e07a5f';
 }
 
 let noEmergencyTasks = document.getElementsByClassName('no-emergency-tasks');
 
-for (i = 0; i < noEmergencyTasks.length; i += 1) {
+for (let i = 0; i < noEmergencyTasks.length; i += 1) {
     noEmergencyTasks[i].style.backgroundColor = '#e9c46a';
 }
 
 let emergency = document.querySelectorAll('.emergency-tasks h3');
 
-for (i = 0; i < emergency.length; i += 1) {  
+for (let i = 0; i < emergency.length; i += 1) {  
     emergency[i].style.backgroundColor = 'blue';
 }
 
 let noEmergency = document.querySelectorAll('.no-emergency-tasks h3');
 
-for (i = 0; i < noEmergency.length; i += 1) {  
+for (let i = 0; i < noEmergency.length; i += 1) {  
     noEmergency[i].style.backgroundColor = 'black';
 }
 
